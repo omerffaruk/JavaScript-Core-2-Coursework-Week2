3f6841253b816393b5b4326535b6c9a0e3c6f817
omerffaruk <farukyamak11044@gmail.com>
2021-07-26 09:43:23 +0100

    Exercises A-list-of-names done

--- exercises/A-list-of-names/script.js
+++ exercises/A-list-of-names/script.js
@@ -1,5 +1,15 @@
 function listOfNames(arrayOfPeople) {
   let content = document.querySelector("#content");
+  arrayOfPeople.forEach((p) => {
+    const h1Name = document.createElement("h1");
+    const h2Job = document.createElement("h2");
+    h1Name.textContent = p.name;
+    h2Job.textContent = p.job;
+    content.appendChild(h1Name);
+    content.appendChild(h2Job);
+    h1Name.style.color = "red";
+    content.style.textAlign = "center"
+  })
 }
 
 let people = [
